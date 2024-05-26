5ed1a20c89491d771212bbd53ca90c5208b6430c
IIRuuy <165058489+IIRuuy@users.noreply.github.com>
2024-05-26 22:37:52 +0700

    Update smoke_test.sh

--- server/smoke_test.sh
+++ server/smoke_test.sh
@@ -5,7 +5,7 @@ deb_pakage/bin/http-server &
 server_pid=$!
 
 timeout 10 nc -z 127.0.0.1 8080 > /dev/null 2>&1
-if [[ $? -eq 0 ]]; then
+if [ $? -eq 0 ]; then
   echo "Сервер запущен успешно!"
   kill $server_pid 
   exit 0
