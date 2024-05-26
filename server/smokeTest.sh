5b7f4c0c12a78245e7c7556f8aa9cba222e5f84a
Daniil Shulgin <manganeko@yandex.ru>
2024-05-26 22:21:41 +0700

    Create smokeTest.sh

---
+++ server/smokeTest.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo dpkg -i http-server-dcsa.deb
+
+deb_pakage/bin/http-server &
+server_pid=$!
+
+timeout 10 nc -z 127.0.0.1 8080 > /dev/null 2>&1
+if [[ $? -eq 0 ]]; then
+  echo "Сервер запущен успешно!"
+  kill $server_pid 
+  exit 0
+else
+  echo "Ошибка запуска сервера!"
+  kill $server_pid
+  exit 1
+fi
